c6b64024bc16cc8c5d1fafc621c30e38f7d4c24c
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-02-24 01:16:39 +0900

    Add Albamn_Hskwakr_Admin_Pages

---
+++ albamn-hskwakr/admin/class-albamn-hskwakr-admin-pages.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * The admin-specific settings pages of the plugin.
+ *
+ * @link       https://github.com/hskwakr/albamn
+ * @since      1.0.0
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ */
+
+/**
+ * The admin-specific settings pages of the plugin.
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ * @author     hskwakr <33633391+hskwakr@users.noreply.github.com>
+ */
+class Albamn_Hskwakr_Admin_Pages
+{
+    /**
+     * Load general settings page.
+     *
+     * @since    1.0.0
+     */
+    public function general_settings(): void
+    {
+    }
+
+    /**
+     * Load instagram importer page.
+     *
+     * @since    1.0.0
+     */
+    public function instagram_importer(): void
+    {
+    }
+}
