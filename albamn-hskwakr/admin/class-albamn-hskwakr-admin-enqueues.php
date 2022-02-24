57b90b1e7f17a44cc64874b738786a9fc63b6dae
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-02-25 05:19:02 +0900

    Add Albamn_Hskwakr_Admin_Enqueues

---
+++ albamn-hskwakr/admin/class-albamn-hskwakr-admin-enqueues.php
@@ -0,0 +1,84 @@
+<?php
+
+/**
+ * The admin-specific enqueues for css/js of the plugin.
+ *
+ * @link       https://github.com/hskwakr/albamn
+ * @since      1.0.0
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ */
+
+/**
+ * The admin-specific enqueues for css/js of the plugin.
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ * @author     hskwakr <33633391+hskwakr@users.noreply.github.com>
+ */
+class Albamn_Hskwakr_Admin_Enqueues
+{
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $albamn_hskwakr    The ID of this plugin.
+     */
+    private $albamn_hskwakr;
+
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $albamn_hskwakr       The name of this plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($albamn_hskwakr, $version)
+    {
+        $this->albamn_hskwakr = $albamn_hskwakr;
+        $this->version = $version;
+    }
+
+    /**
+     * Register the stylesheets for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function styles(): void
+    {
+        wp_enqueue_style(
+            $this->albamn_hskwakr,
+            plugin_dir_url(__FILE__) . 'css/albamn-hskwakr-admin.css',
+            array(),
+            $this->version,
+            'all'
+        );
+    }
+
+    /**
+     * Register the JavaScript for the admin area.
+     *
+     * @since    1.0.0
+     */
+    public function scripts(): void
+    {
+        wp_enqueue_script(
+            $this->albamn_hskwakr,
+            plugin_dir_url(__FILE__) . 'js/albamn-hskwakr-admin.js',
+            array( 'jquery' ),
+            $this->version,
+            false
+        );
+    }
+}
