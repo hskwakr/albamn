ab21d74594836069426f3f8045e832716280d8fd
hskwakr <33633391+hskwakr@users.noreply.github.com>
2022-06-09 10:07:36 +0900

    Add Ig_Shortcode class

---
+++ albamn-hskwakr/ig/class-albamn-hskwakr-ig-shortcode.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * The context for Instagram API.
+ *
+ * @link       https://github.com/hskwakr/albamn
+ * @since      1.0.0
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ */
+
+/**
+ * The context for Instagram API.
+ *
+ * @package    Albamn_Hskwakr
+ * @subpackage Albamn_Hskwakr/admin
+ * @author     hskwakr <33633391+hskwakr@users.noreply.github.com>
+ */
+class Albamn_Hskwakr_Ig_Shortcode
+{
+    /**
+     * The formatter for Instagram medias
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      Albamn_Hskwakr_Admin_Ig_Formatter    $formatter
+     */
+    private $formatter;
+
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param    Albamn_Hskwakr_Admin_Ig_Formatter    $formatter
+     */
+    public function __construct(
+        Albamn_Hskwakr_Admin_Ig_Formatter $formatter
+    ) {
+        $this->formatter = $formatter;
+    }
+
+    /**
+     * Run the shortcode of the plugin
+     *
+     * @since    1.0.0
+     * @return   string
+     */
+    public function run_shortcode(): string
+    {
+        return '';
+    }
+}
